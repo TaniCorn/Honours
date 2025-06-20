3a4177a7303672227b09b55c959ce098b1b32cdf
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2025-06-20 08:12:36 +0100

    rework-structure
    Reworked the project structure to seperate public .h files and source files with private .h. I've done this because I could see it getting cumbersome when trying to do multiple relative includes.

---
+++ SparseVoxelOctreeRenderer/Include/Shaders/Texture/Texture_ps.hlsl
@@ -0,0 +1,15 @@
+Texture2D texture0 : register(t0);
+SamplerState Sampler0 : register(s0);
+
+struct InputType
+{
+    float4 position : SV_POSITION;
+    float2 tex : TEXCOORD0;
+};
+
+float4 main(InputType input) : SV_TARGET
+{
+    // This will simply sample the texture, this should recieve the raytraced svo
+    float4 col = texture0.Sample(Sampler0, input.tex);
+    return col;
+}
---
+++ SparseVoxelOctreeRenderer/Include/Voxels/Voxel.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef VOXEL_H
+#define VOXEL_H
+
+#include "DXF.h"
+
+struct Voxel
+{
+	XMFLOAT3 Point;
+	UINT32 Color;
+};
+
+#endif
